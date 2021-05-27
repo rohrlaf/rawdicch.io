cdd0ecbe972bdc657c338dea0b9507ec53401818
rohrlaf <robert@medium-rare.io>
2021-05-27 15:26:04 +0200

    Adjust component generator

--- generate/react-component/react-component.js
+++ generate/react-component/react-component.js
@@ -1,22 +1,30 @@
 module.exports = {
   description: "Generates new React component",
-  prompts : [{
-    type: "input",
-    name: "name",
-    message: "What is the component's name?",
-    validate: (value) => {
-      let message = true;
-      if(!/.+/.test(value)) {
-        message = console.error("Missing", "you must define a component name")
-      } else if (value.length < 3) {
-        message = console.error("your", `"${value}, name is too short`)
+  prompts : [
+    {
+      type: "input",
+      name: "name",
+      message: "Component name:",
+      validate: (value) => {
+        let message = true;
+        if(!/.+/.test(value)) {
+          message = console.error("Missing", "you must define a component name")
+        } else if (value.length < 3) {
+          message = console.error("your", `"${value}, name is too short`)
+        }
+        return message;
       }
-      return message;
-    }
-  }],
+    },
+    {
+      type: "rawlist",
+      name: "package",
+      message: "Package:",
+      choices: ["components", "client"]
+    },
+  ],
   actions: () => [{
     type: "add",
-    path: "./components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+    path: "./packages/{{package}}/src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
     templateFile: "./generate/react-component/templates/component.hbs"
   }]
 }
