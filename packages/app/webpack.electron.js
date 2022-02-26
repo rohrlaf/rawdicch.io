17cc7b5aaa09605adbd65d97c088c3631d2babb0
rohrlaf <robert@medium-rare.io>
2022-02-26 16:32:14 +0100

    refactor: simplify electron webpack

--- packages/app/webpack.electron.js
+++ packages/app/webpack.electron.js
@@ -1,35 +1,38 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
+const common_dev = {
+  resolve: {
+    alias: {
+      ['@']: path.resolve(__dirname, 'src'),
+      '@prisma/client$': require.resolve('@prisma/client'),
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [/src/],
+        use: [{ loader: 'ts-loader' }],
+      },
+    ],
+  },
+  externals: {
+    _http_common: 'commonjs2 _http_common',
+    encoding: 'commonjs2 encoding',
+  },
+};
+
 module.exports = [
   {
+    ...common_dev,
     mode: 'development',
     entry: './src/core/electronMain.ts',
     target: 'electron-main',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'main.js',
-    },
-    node: { global: true },
-    resolve: {
-      alias: {
-        ['@']: path.resolve(__dirname, 'src'),
-        '@prisma/client$': require.resolve('@prisma/client'),
-      },
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          include: [/src/],
-          use: [{ loader: 'ts-loader' }],
-        },
-      ],
-    },
-    externals: {
-      _http_common: 'commonjs2 _http_common',
-      encoding: 'commonjs2 encoding',
     },
     plugins: [
       new CopyWebpackPlugin({
@@ -43,32 +46,13 @@ module.exports = [
     ],
   },
   {
+    ...common_dev,
     mode: 'development',
     entry: './src/core/electronPreload.ts',
     target: 'electron-preload',
     output: {
       path: path.join(__dirname, 'dist'),
       filename: 'preload.js',
-    },
-    resolve: {
-      alias: {
-        ['@']: path.resolve(__dirname, 'src'),
-        '@prisma/client$': require.resolve('@prisma/client'),
-      },
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          include: [/src/],
-          use: [{ loader: 'ts-loader' }],
-        },
-      ],
-    },
-    externals: {
-      _http_common: 'commonjs2 _http_common',
-      encoding: 'commonjs2 encoding',
     },
   },
 ];
